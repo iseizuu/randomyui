992f53d605b809a6b3499e081b7fb63bb29ae9b7
VeguiIzumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-08-20 10:54:58 +0700

    Update endpoints.js

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,20 +1,20 @@
 const fetch = require('node-superfetch')
-const baseURL = 'https://nezumiyuiz.glitch.me/api/'
+const baseURL = 'https://nezumiapi.veguiizumi.repl.co/api/'
 
 class randomyuiz {
 
     async animeImage(choice) {
         const list = ["anime", "chika", "hayasaka", "kaguyashinomiya", "megumin", "oyasumi", "rem", "zerotwo"]
         if(!list.find(a => a === choice.toLowerCase())) {
-            return console.log('Please enter one of the following options :' + list.join(", "))
+            throw new TypeError('Please enter one of the following options :' + list.join(", "))
         }
         const res = await fetch.get(baseURL + choice.toLowerCase())
         const data = res.body.url;
 
         if(res.status !== 200) {
-            return console.log('Api is currently down')
+            throw new TypeError('Api is currently down')
         }
-
+        
         return data;
 
     }
@@ -23,13 +23,13 @@ class randomyuiz {
         const pict = imageURL || 'https://cdn.discordapp.com/attachments/688763072864976906/736122806600859789/chika.png'
         const list = ["grayscale", "invert", "sepia"]
         if(!list.find(a => a === choice.toLowerCase())) {
-            return console.log('Please enter one of the following options : ' + list.join(", "))
+            throw new TypeError('Please enter one of the following options : ' + list.join(", "))
         }
         const res = await fetch.get(`${baseURL}${choice.toLowerCase()}?image=${pict}`);
         const data = res.url;
 
         if(res.status !== 200) {
-            return console.log('Api is currently down')
+            throw new TypeError('Api is currently down')
         };
 
         return data;
